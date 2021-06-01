4c3cdbf1ad2efbcc5a178b873c5435d006e321e4
AlacranBlanco <brauliome98@gmail.com>
2021-05-31 21:21:43 -0500

    cambios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -106,8 +106,15 @@ const actualizarUsuario = async (req, res = response) => {
                 });
             }
         }
+        if (!usuarioDB.google) {
+            campos.email = email;
+        } else if (usuarioDB.email !== email) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Este usuario no puede cambiar su correo'
+            });
+        }
 
-        campos.email = email;
         const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, {new: true});
 
         res.json({
