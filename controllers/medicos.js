2433ac849838dc8b9d60bad2c28fa008e81a4ed9
AlacranBlanco <brauliome98@gmail.com>
2021-06-02 19:42:33 -0500

    cambios

--- controllers/medicos.js
+++ controllers/medicos.js
@@ -5,14 +5,36 @@ const Medico = require('../models/medico');
 const getMedicos = async (req, res = response) => {
 
     const medicos = await Medico.find()
-        .populate('usuario', 'nombre img')
-        .populate('hospital', 'nombre img')
+        .populate('usuario', 'name img')
+        .populate('hospital', 'name img')
 
 
     res.json({
         ok: true,
         medicos
     })
+}
+
+const getMedicoById = async (req, res = response) => {
+    const id = req.params.id;
+
+    try {
+        const medico = await Medico.findById(id)
+            .populate('usuario', 'name img')
+            .populate('hospital', 'name img')
+
+
+        res.json({
+            ok: true,
+            medico
+        })
+    } catch (e) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
+
 }
 
 const crearMedico = async (req, res = response) => {
@@ -111,5 +133,6 @@ module.exports = {
     getMedicos,
     crearMedico,
     actualizarMedico,
-    borrarMedico
+    borrarMedico,
+    getMedicoById
 }
